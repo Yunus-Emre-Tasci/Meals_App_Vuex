19c5823dd3ebd964b77b1eb2f03ec51f9a3410d3
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-23 11:58:07 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,5 @@
 <template>
-  <div class="bg-gray-100 h-full">
-    <header class="bg-white shadow h-16 flex justify-between items-center px-5">
-      <router-link :to="{name:'home'}">
-        Home
-      </router-link>
-      <div class="flex gap-2">
-        <router-link :to="{name:'byName'}" class="px-3 py-2 rounded h-full transition-colors hover:bg-purple-200">
-          Search Meals
-        </router-link>
-        <router-link :to="{name:'byLetter'}" class="px-3 py-2 rounded h-full transition-colors hover:bg-purple-200">
-          Meals By Letter
-        </router-link>
-        <router-link :to="{name:'byIngredient'}" class="px-3 py-2 rounded h-full transition-colors hover:bg-purple-200">
-          Meals By Ingredients
-        </router-link>
-      </div>
-    </header>
-    <main>
-      <router-view/>
-    </main>
-  </div>
+  <router-view/>
 </template>
 
 <script setup>
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,8 +1,19 @@
 <template>
-    <div>
-        By Name
+    <div class="p-6">
+        <input type="text" v-model="keyword" class="rounded border-2 border-gray-200 w-full" placeholder="Search for Meals" @change="searchMeals"/>
     </div>
+    <!-- <pre>{{ meals }}</pre> -->
 </template>
 
 <script setup>
+import { ref } from "vue";
+import store from "../store"
+
+const keyword=ref("")
+const meals=computed(()=>store.state.searchMeals)
+
+function searchMeals(){
+   store.dispatch("searchMeals",keyword.value)
+}
+
 </script>
