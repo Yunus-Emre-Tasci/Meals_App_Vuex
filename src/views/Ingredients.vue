73868bceb91923e477835ba3476c6d6fc71d2341
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2023-06-26 09:13:53 +0300

    Ingredient page

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,9 +1,15 @@
 <template>
-    
+   <div class="p-8">
+       <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+      <router-link :to="{name:'byIngredient', params:{ingredient}}" class="bg-white block rounded p-3 mb-3 shadow" :key="ingredient.idIngredient" v-for="ingredient of ingredients">
+      <h3 class="text-2xl font-bold mb-3">{{ ingredient.strIngredient }}</h3>
+      <p>{{ ingredient.strDescription }}</p>
+    </router-link>
+    </div> 
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted,ref } from "vue";
 import axiosClient from "../../axiosClient";
 
 
